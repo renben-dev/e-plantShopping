5959bc3fe9e67e458676246a9ef968ad0aa54a8d
renben-dev <bidone.renzo@gmail.com>
2025-04-30 12:16:35 -0400

    rolling update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,7 +274,27 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                {plantsArray.map((plantCategory,index) => (
+                    <div> key = {index} 
+                        <h1>{plantCategory.category}</h1>
+                        <div className='product-list'>
+                        {plantCategory.plants.map((plant, plantIndex) => (
+                            <div className='product-card' key={plantIndex}>
+                                <img className='product-image' src={plant.image} alt={plant.name} />    
+                                <div className='product-title'>{plant.name}</div>
+                                <div className='product-description'>{plant.description}</div>
+                                <div className='product-price'>{plant.price}</div>
 
+                                <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+
+                            </div>
+
+                        ))}
+
+                        </div>
+    
+                    </div>
+                ))}
 
                 </div>
             ) : (
